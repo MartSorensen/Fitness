036121ad4d63da3a60807d1f9f8beee702154fa4
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2020-05-06 17:09:57 +0200

    add workout works

--- app/src/main/java/com/example/fitnessapp/ui/workouts/WorkoutsFragment.java
+++ app/src/main/java/com/example/fitnessapp/ui/workouts/WorkoutsFragment.java
@@ -4,13 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -19,7 +17,7 @@ import com.example.fitnessapp.R;
 public class WorkoutsFragment extends Fragment {
 
     private WorkoutsViewModel workoutsViewModel;
-    Button addWorkoutButton;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,18 +33,7 @@ public class WorkoutsFragment extends Fragment {
             }
         });
 
-        addWorkoutButton = root.findViewById(R.id.addWorkoutButton);
-        addWorkoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AddWorkoutFragment addWorkoutFragment = new AddWorkoutFragment();
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                transaction.replace(R.id.container,addWorkoutFragment);
-
-                transaction.commit();
-            }
-        });
-
         return root;
     }
+
 }
