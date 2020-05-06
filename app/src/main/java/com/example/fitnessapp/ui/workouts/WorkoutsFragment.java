3e8e3f91c0c45340be2a2e85498a8a04e63f28b8
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2020-05-06 15:04:26 +0200

    workouts tab

--- app/src/main/java/com/example/fitnessapp/ui/workouts/WorkoutsFragment.java
+++ app/src/main/java/com/example/fitnessapp/ui/workouts/WorkoutsFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -17,6 +19,7 @@ import com.example.fitnessapp.R;
 public class WorkoutsFragment extends Fragment {
 
     private WorkoutsViewModel workoutsViewModel;
+    Button addWorkoutButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,8 +31,22 @@ public class WorkoutsFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+
             }
         });
+
+        addWorkoutButton = root.findViewById(R.id.addWorkoutButton);
+        addWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddWorkoutFragment addWorkoutFragment = new AddWorkoutFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.container,addWorkoutFragment);
+
+                transaction.commit();
+            }
+        });
+
         return root;
     }
 }
